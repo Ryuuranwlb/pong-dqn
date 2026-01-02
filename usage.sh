d30803a6ffff6095b5d233812c525d7210c2331f
Ryuuran <klpeng@bjtu.edu.cn>
2026-01-03 00:41:06 +0800

    modified:   .gitignore 	modified:   usage.sh

--- usage.sh
+++ usage.sh
@@ -3,33 +3,10 @@
 python multiplayer.py \
     -agent_1 D3QN \
     -total_episode 400 \
-    -memo_1 d3qn_huber_nstep \
-    -run_root runs_bef_nstep/newer \
+    -player 2 \
+    -memo_1 l_d3qn_huber_default \
+    -memo_2 r_teacher_ori \
+    -start_step_2 23333 \
+    -run_root final_submit \
     --loss huber \
-    --n_step 3
-
-python multiplayer.py \
-    -agent_1 DuelingDQN \
-    -total_episode 400 \
-    -memo_1 duelingdqn_huber_nstep \
-    -run_root runs_bef_nstep/newer \
-    --loss huber \
-    --n_step 3
-
-
-python multiplayer.py \
-    -agent_1 D3QN \
-    -total_episode 400 \
-    -memo_1 d3qn_huber_shaping \
-    -run_root runs_bef_nstep/newer \
-    --loss huber \
-    --reward_shaping score_delta 
-
-python multiplayer.py \
-    -agent_1 D3QN \
-    -total_episode 400 \
-    -memo_1 d3qn_huber_nstep_shaping \
-    -run_root runs_bef_nstep/newer \
-    --loss huber \
-    --n_step 3 \
-    --reward_shaping score_delta 
+    --train_left
